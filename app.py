3c5fdbcfdcef90bc2b0067893420819e30078a70
Er. Ashwin Nair <ashwin.nair0309@gmail.com>
2025-10-14 21:33:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ load_dotenv()
 # Configure Gemini API
 # -----------------------------
 api_key = os.getenv("API_KEY")
-genai.configure(api_key)
+genai.configure(api_key=api_key)
 model = genai.GenerativeModel("models/gemini-2.5-flash")
 
 # -----------------------------
@@ -715,5 +715,6 @@ elif task == "Interactive Quiz Mode":
                         suggestion_text = safe_generate(suggestion_prompt)
                         if suggestion_text:
                             st.markdown(f"💡 Study Tip: {suggestion_text}")
+
 
 
